0ce2ae619ce29366d181d649a2f496c6703dec0c
Thiago Omoto <thi.omoto@gmail.com>
2023-07-21 13:35:55 -0300

    criando a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,21 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aaqui"""
-    raise NotImplementedError
+    try:
+        time.sleep(1)
+        response = requests.get(url,
+                                headers={"user-agent": "Fake user-agent"},
+                                timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
